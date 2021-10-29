00522ea79a7644bfc67f9429799010d5bdfc00c1
MARI <mari20201101@gmail.com>
2021-10-30 00:09:33 +0900

    Test string replace

--- src/test/java/com/mariworld/async/AsyncStudyApplicationTests.java
+++ src/test/java/com/mariworld/async/AsyncStudyApplicationTests.java
@@ -4,8 +4,15 @@ import netscape.javascript.JSObject;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriBuilder;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+import java.net.URL;
 
 @SpringBootTest
 class AsyncStudyApplicationTests {
@@ -18,10 +25,6 @@ class AsyncStudyApplicationTests {
 	}*/
 
 	@Test
-	void contextLoads() {
-	}
-
-	@Test
 	public void stringObjectTest() throws Exception {
 
 		long start = System.nanoTime();
@@ -30,7 +33,7 @@ class AsyncStudyApplicationTests {
 			url+="/hello";
 			url+="/v2";
 			url+="/spring";
-			ResponseEntity<JSObject> result = restTemplate.getForEntity(url, JSObject.class);
+			Object result = restTemplate.getForObject(url, Object.class);
 			System.out.println(result);
 		}
 		long end = System.nanoTime();
@@ -43,7 +46,7 @@ class AsyncStudyApplicationTests {
 		for (int i = 0; i < 100; i++) {
 			URL.replace("{hello}","hello");
 			URL.replace("{spring}","spring");
-			ResponseEntity<JSObject> result = restTemplate.getForEntity(URL, JSObject.class);
+			Object result = restTemplate.getForObject(URI.create(URL), Object.class);
 			System.out.println(result);
 		}
 		long end = System.nanoTime();
